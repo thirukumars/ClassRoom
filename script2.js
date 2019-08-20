214b79580e034f49bacf5cd5deac3fc4a3878995
praveenkumar37 <39609315+thirukumars@users.noreply.github.com>
2019-08-20 10:14:00 +0530

    mapping names and Expression
    It has some modification in the face-api.js file for mapping names and expression (drawFaceExpression() function in the face-api.js)

--- script2.js
+++ script2.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-run()
+  run()
 })
+
+//load all the models ./models
 async function run(){
   Promise.all([
   await faceapi.loadSsdMobilenetv1Model('./models'),
@@ -8,83 +10,132 @@ async function run(){
   await faceapi.nets.faceExpressionNet.loadFromUri('/models'),
   await faceapi.loadFaceLandmarkModel('./models'),
   await faceapi.loadFaceRecognitionModel('./models')
- ])
-    const videoEl=document.getElementById('inputVideo')
-    navigator.getUserMedia(
-        {video:{}},
-        stream=>videoEl.srcObject=stream,
-        err=>console.log(err)
-    )
-    onPlay(videoEl)
+])
+
+//once we loaded the models start the Video(WebCam)
+const videoEl=document.getElementById('inputVideo')
+  navigator.getUserMedia(
+    {video:{}},
+      stream=>videoEl.srcObject=stream,
+      err=>console.log(err)
+  )
+  onPlay(videoEl)//calling the onPlay()
 }
-var time=0;
-var arr=[];
 
+//global declarations
+var NameArray=[];
+var ExpressionArray=[];
+var count=0;
+var expression;
+var max=0;
+var map=new Map();
+
+//The main onplay function for faceDetection and Expressions
 async function onPlay(videoEl){
-    // console.log('it is running');
-    const canvas =document.getElementById('overlay');
-    const displaySize = { width: videoEl.width, height: videoEl.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    // console.log('it is running 2')
-        
-    const options = new faceapi.TinyFaceDetectorOptions()
-    let fullFaceDescriptions = await faceapi.detectAllFaces(videoEl, options).withFaceLandmarks().withFaceExpressions().withFaceDescriptors()
-    fullFaceDescriptions=faceapi.resizeResults(fullFaceDescriptions,displaySize)
-    const labels = ['praveen','varun_sir','boobal','dhaya']
-    const labeledFaceDescriptors = await Promise.all(
-    labels.map(async label => {
-    // fetch image data from urls and convert blob to HTMLImage element
-    const imgUrl = `./${label}.jpg`
-    const img = await faceapi.fetchImage(imgUrl)
-    
-    // detect the face with the highest score in the image and compute it's landmarks and face descriptor
-    const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceExpressions().withFaceDescriptor()
-    // console.log(fullFaceDescription.expressions.asSortedArray().toString().expression);
-    if (!fullFaceDescription) { 
+  const canvas =document.getElementById('overlay');
+  const displaySize = { width: videoEl.width, height: videoEl.height }
+  faceapi.matchDimensions(canvas, displaySize)        
+
+  //This is the detection option(TinyFace,mtcnn,singleFaceDetection)
+  const options = new faceapi.TinyFaceDetectorOptions()
+
+  //This will detect all the images in the webCam
+  let fullFaceDescriptions = await faceapi.detectAllFaces(videoEl, options).withFaceLandmarks().withFaceExpressions().withFaceDescriptors()
+  
+  //resizing the detected face to the displaySize
+  fullFaceDescriptions=faceapi.resizeResults(fullFaceDescriptions,displaySize)
+  
+  //Names of labels(Project Directory)
+  const labels = ['praveen','varun_sir']
+  
+  /*This labeledFaceDescriptor is used for the mapping images to the detected face in fullFaceDescription
+    and also check the webcam images contain face or not.
+  */
+  const labeledFaceDescriptors = await Promise.all( 
+    labels.map(async label => { 
+
+      // fetch image data from urls and convert blob to HTMLImage element
+      const imgUrl = `./${label}.jpg`
+      const img = await faceapi.fetchImage(imgUrl)
+     
+      // detect the face with the highest score in the image and compute it's landmarks and face descriptor
+      const fullFaceDescription = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceExpressions().withFaceDescriptor()
+
+     if (!fullFaceDescription) { 
       throw new Error(`no faces detected for ${label}`)
-    }
+      }
     
-    const faceDescriptors = [fullFaceDescription.descriptor]
-    return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
-  })
-)  
-    const maxDescriptorDistance = 0.6
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
+     const faceDescriptors = [fullFaceDescription.descriptor]
+        return new faceapi.LabeledFaceDescriptors(label, faceDescriptors)
+    })
+  )  
 
-    const results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
-    let count=0;
-    results.forEach((bestMatch, i) => {
-    count++;
-    const box = fullFaceDescriptions[i].detection.box
-    const text = bestMatch.toString()
+
+  const maxDescriptorDistance = 0.6
+  //faceMatcher take the highest confidence and refernce and query image.
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, maxDescriptorDistance)
+  count++;
+  console.log("outside"+count);
+
+  //the result holds the complete mapping of refernce and query images(by using the Array values)
+  results = fullFaceDescriptions.map(fd => faceMatcher.findBestMatch(fd.descriptor))
+  
+  //This iterates give the detected faces in images
+  results.forEach((bestMatch, i) => {
+    console.log(bestMatch);
+    const box = fullFaceDescriptions[i].detection.box;
+    const text = bestMatch.toString();  //this for basMatch name detection
+    console.log("inside"+count);
     var str=""
-    for(var i of text){
-      if(i!='(')
-      str+=i;
-      else
-      break;
-    }
-    if(arr.includes(str)===false)
-    arr.push(str)
-    //console.log(`${count}. ${text}`);
+      for(var i of text){
+        if(i!='('){
+        str+=i;
+        }
+        else{
+          break;
+        }
+      }
+      if(NameArray.includes(str)===false)
+        NameArray.push(str);
+        // console.log(str);
     const drawBox = new faceapi.draw.DrawBox(box, { label: text })
-  // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-  // faceapi.draw.drawDetections(canvas, fullFaceDescriptions)
-    const result_1=faceapi.draw.drawFaceExpressions(canvas, fullFaceDescriptions)
-    // console.log(result_1.detection)
-    // var drawTextField = new draw.DrawTextField(resultsToDisplay.map(function (result_1) { return expr.expression}))
-  //console.log(fullFaceDescriptions[i].expressions.asSortedArray().__proto__.toString())
+      // faceapi.draw.drawFaceExpressions(canvas, fullFaceDescriptions)//FaceExpression=fullFaceDescription  *(face-api.js)*
     drawBox.draw(canvas);
-})
-var call=setTimeout( () => {onPlay(videoEl)
-    },200)
-// time++;
-// console.log(time);
-// if(time==5){
-//   clearInterval(call);
-//    console.log(count+' person detected'); 
-   for(let i=0;i<arr.length;i++){
-  console.log(arr[i]) 
-   }
-  }
+  })
+  
+  //marks the expression for the each values in result Arrays.
+  faceapi.draw.drawFaceExpressions(canvas, fullFaceDescriptions,NameArray) //(sending NameArray for mapping names and expression)
+  
+  //SetTimeout for repeated detection of faces
+  var call=setTimeout( () => {
+    onPlay(videoEl)
+  },200)
 
+  if(count>=10){
+  clearInterval(call);
+  }
+}
+
+function test(e,Name){
+  console.log(e+" how is it");
+  console.log(Name);
+  NameArray=[];
+  // if(NameArray.includes(f)===false)
+  // NameArray.push(f);
+  ExpressionArray.push(e)
+    if(count==10){
+     max=0;
+      for(let i=0;i<ExpressionArray.length;i++){
+        let counter=0;
+          for(let j=0;j<ExpressionArray.length;j++){
+            if(ExpressionArray[i]==ExpressionArray[j]){
+                counter++;
+            }
+          }
+          if(counter>max){
+            max=counter;
+            expression=ExpressionArray[i];
+          }
+      }
+    }
+}
